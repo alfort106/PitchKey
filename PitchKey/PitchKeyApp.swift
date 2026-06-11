1a5bc15064d9edadd5ca48fb57f1818136baba42
alfort106 <alfort106@gmail.com>
2026-06-11 11:50:46 +0900

    Initial Commit

---
+++ PitchKey/PitchKeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PitchKeyApp.swift
+//  PitchKey
+//
+//  Created by arakawa hinata on 2026/06/11.
+//
+
+import SwiftUI
+
+@main
+struct PitchKeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
